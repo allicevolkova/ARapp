4b1c2cfaf896fb2940ec2a29286a23c6646d8372
allicevolkova <allicevolkova@gmail.com>
2022-03-23 18:05:16 +0200

    Changes

--- app/src/main/java/com/example/ar_tourism_app/MainActivity.java
+++ app/src/main/java/com/example/ar_tourism_app/MainActivity.java
@@ -37,6 +37,9 @@ public class MainActivity extends AppCompatActivity implements
     private final List<CompletableFuture<Void>> futures = new ArrayList<>();
     private ArFragment arFragment;
     private boolean doorDetected = false;
+    private boolean churchDetected = false;
+    private boolean lighthouseDetected = false;
+    private boolean museumDetected = false;
     //
 
 
@@ -80,6 +83,15 @@ public class MainActivity extends AppCompatActivity implements
         // Every image has to have its own unique String identifier
         database.addImage("door", doorImage);
 
+        Bitmap churchImage = BitmapFactory.decodeResource(getResources(), R.drawable.church);
+        database.addImage("church", churchImage);
+
+        Bitmap lighthouseImage = BitmapFactory.decodeResource(getResources(), R.drawable.lighthouse);
+        database.addImage("lighthouse", lighthouseImage);
+
+        Bitmap museumImage = BitmapFactory.decodeResource(getResources(), R.drawable.museum);
+        database.addImage("museum", museumImage);
+
         config.setAugmentedImageDatabase(database);
 
         // Check for image detection
@@ -98,7 +110,7 @@ public class MainActivity extends AppCompatActivity implements
 
     public void onAugmentedImageTrackingUpdate(AugmentedImage augmentedImage) {
         // If there are both images already detected, for better CPU usage we do not need scan for them
-        if (doorDetected) {
+        if (doorDetected || churchDetected || lighthouseDetected || museumDetected) {
             return;
         }
 
@@ -118,7 +130,7 @@ public class MainActivity extends AppCompatActivity implements
                 arFragment.getArSceneView().getScene().addChild(anchorNode);
 
                 futures.add(ModelRenderable.builder()
-                        .setSource(this, Uri.parse("models/door_two.glb"))
+                        .setSource(this, Uri.parse("models/door.glb"))
                         .setIsFilamentGltf(true)
                         .build()
                         .thenAccept(doorModel->{
@@ -134,8 +146,81 @@ public class MainActivity extends AppCompatActivity implements
                                     return null;
                                 }));
             }
+            if (!churchDetected && augmentedImage.getName().equals("church")) {
+                churchDetected = true;
+                Toast.makeText(this, "Church tag detected", Toast.LENGTH_LONG).show();
+
+                anchorNode.setWorldScale(new Vector3(3.5f, 3.5f, 3.5f));
+                arFragment.getArSceneView().getScene().addChild(anchorNode);
+
+                futures.add(ModelRenderable.builder()
+                        .setSource(this, Uri.parse("models/church.glb"))
+                        .setIsFilamentGltf(true)
+                        .build()
+                        .thenAccept(churchModel->{
+                            TransformableNode modelNode = new TransformableNode(arFragment.getTransformationSystem());
+                            modelNode.getScaleController().setMaxScale(0.003f);
+                            modelNode.getScaleController().setMinScale(0.002f);
+                            modelNode.setRenderable(churchModel);
+                            anchorNode.addChild(modelNode);
+                        })
+                        .exceptionally(
+                                throwable -> {
+                                    Toast.makeText(this, "Unable to load church model", Toast.LENGTH_LONG).show();
+                                    return null;
+                                }));
+            }
+            if (!lighthouseDetected && augmentedImage.getName().equals("lighthouse")) {
+                lighthouseDetected = true;
+                Toast.makeText(this, "Lighthouse tag detected", Toast.LENGTH_LONG).show();
+
+                anchorNode.setWorldScale(new Vector3(3.5f, 3.5f, 3.5f));
+                arFragment.getArSceneView().getScene().addChild(anchorNode);
+
+                futures.add(ModelRenderable.builder()
+                        .setSource(this, Uri.parse("models/lighthouse.glb"))
+                        .setIsFilamentGltf(true)
+                        .build()
+                        .thenAccept(lighthouseModel->{
+                            TransformableNode modelNode = new TransformableNode(arFragment.getTransformationSystem());
+                            modelNode.getScaleController().setMaxScale(0.006f);
+                            modelNode.getScaleController().setMinScale(0.005f);
+                            modelNode.setRenderable(lighthouseModel);
+                            anchorNode.addChild(modelNode);
+                        })
+                        .exceptionally(
+                                throwable -> {
+                                    Toast.makeText(this, "Unable to load lighthouse model", Toast.LENGTH_LONG).show();
+                                    return null;
+                                }));
+            }
+            if (!museumDetected && augmentedImage.getName().equals("museum")) {
+                museumDetected = true;
+                Toast.makeText(this, "Museum tag detected", Toast.LENGTH_LONG).show();
+
+                anchorNode.setWorldScale(new Vector3(3.5f, 3.5f, 3.5f));
+                arFragment.getArSceneView().getScene().addChild(anchorNode);
+
+                futures.add(ModelRenderable.builder()
+                        .setSource(this, Uri.parse("models/museum.glb"))
+                        .setIsFilamentGltf(true)
+                        .build()
+                        .thenAccept(museumModel->{
+                            TransformableNode modelNode = new TransformableNode(arFragment.getTransformationSystem());
+                            modelNode.getScaleController().setMaxScale(0.003f);
+                            modelNode.getScaleController().setMinScale(0.002f);
+                            modelNode.setRenderable(museumModel);
+                            anchorNode.addChild(modelNode);
+                        })
+                        .exceptionally(
+                                throwable -> {
+                                    Toast.makeText(this, "Unable to load museum model", Toast.LENGTH_LONG).show();
+                                    return null;
+                                }));
+            }
         }
-        if (doorDetected) {
+
+        if (doorDetected || churchDetected || lighthouseDetected || museumDetected) {
             arFragment.getInstructionsController().setEnabled(
                     InstructionsController.TYPE_AUGMENTED_IMAGE_SCAN, false);
         }
